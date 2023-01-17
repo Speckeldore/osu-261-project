f73254244ae922da042456d503559c84dc9b5dc4
Speckeldore <santossk@oregonstate.edu>
2023-01-17 11:21:09 -0800

    hello world

---
+++ hello_gradescope.py
@@ -0,0 +1,9 @@
+# Name: Skyler Santos
+# OSU Email: santossk@oregonestate.edu
+# Course: CS261 - Data Structures
+# Assignment: 0
+# Due Date: 1/18
+# Description: gradescope test
+def gradescope_test():
+    return 42
+
